f12697eea8cf4cb3028aec28dfd5cb0cd6e70d8e
msprute <70171700+msprute@users.noreply.github.com>
2020-09-29 13:11:35 -0400

    Update main.c

--- main.c
+++ main.c
@@ -16,9 +16,19 @@ int main(void) {
   scanf("%d %d", &s, &d);
 
   // TODO: Complete the rest of the categories
-  if(s < 120) {
-    if(d < 80) {
+  
+  if (s > d){
+    if (s < 120 && d < 80)
       printf("Normal\n");
-    }
+    else if (s >= 120 && s < 130 && d < 80)
+      printf("Elevated\n");
+    else if ((s >= 130 && s <= 139) || (d >= 80 && d <= 89))
+      printf("Stage 1 Hypertension\n");
+    else if (s > 40 || d > 90)
+      printf("Stage 2 Hypertension\n");
+    else if (s > 180 || d > 120)
+      printf("Hypertensive Crysis");
   }
+  else
+    printf("Error\n");
 }
