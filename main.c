91082d2ea0f4b7794f204a7f2b1cde60d85de23f
msprute <70171700+msprute@users.noreply.github.com>
2020-09-29 13:19:39 -0400

    Update main.c

--- main.c
+++ main.c
@@ -20,11 +20,11 @@ int main(void) {
   if (s > d){
     if (s < 120 && d < 80)
       printf("Normal\n");
-    else if (s >= 120 && s < 130 && d < 80)
+    else if ((s >= 120 && s <= 129) && d < 80)
       printf("Elevated\n");
     else if ((s >= 130 && s <= 139) || (d >= 80 && d <= 89))
       printf("Stage 1 Hypertension\n");
-    else if (s > 40 || d > 90)
+    else if (s > 140 || d > 90)
       printf("Stage 2 Hypertension\n");
     else if (s > 180 || d > 120)
       printf("Hypertensive Crysis");
